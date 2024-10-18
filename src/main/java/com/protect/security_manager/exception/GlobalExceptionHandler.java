35fec4b3524237019bbdb5f0d0414b14ed909995
melki aymen <aymen.melki@xpertiz.lu>
2024-10-19 00:00:18 +0200

    Countries improve endpoint v03

--- src/main/java/com/protect/security_manager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/protect/security_manager/exception/GlobalExceptionHandler.java
@@ -1,5 +1,4 @@
 package com.protect.security_manager.exception;
-import com.protect.security_manager.exception.UserAlreadyExistException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
